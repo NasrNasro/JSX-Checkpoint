d465a416dd18eaa053b85500fdad0418689157f9
Nassreddine Hnana <nasrihnana@gmail.com>
2021-11-08 00:05:18 +0100

    Adding checkpoint JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import img from './imageInSrc.jfif';
+import React from 'react';
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <React.Fragment>
+    <div style={{ border : "solid 1px black", maxWidth : "100vw" }}>
+      <h1 className = 'title-red'>Nassreddine Hnana</h1>
+      <br/>
+      <img src = {img} alt = "gi" />
+      <br/>
+      <img src = "/imageInPublic.jfif" alt = "gi" />
     </div>
+    <video width="320" height="240" controls>
+    <source src="myVideo.mp4" type="video/mp4" />
+    </video>
+    </React.Fragment>
   );
 }
 
